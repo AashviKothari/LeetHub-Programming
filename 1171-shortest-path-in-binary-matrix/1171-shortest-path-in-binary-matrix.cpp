72d958be906dc7ac613073a1c53dff17fa169e82
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-06-11 14:24:19 +0530

    Time: 63 ms (90.84%), Space: 19.7 MB (66.88%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
-        int n=g.size();
-        if(g[0][0] || g[n-1][n-1])return -1;
-        g[0][0]=1;
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        int t=0;
-        while(q.size()){
-            int k=q.size();
-            t++;
-            while(k--){
-                int x=q.front().first;
-                int y=q.front().second;
-                q.pop();
-                if(x==n-1 && y==n-1)return t;
-                int dx[8]={1,-1,0,0,1,1,-1,-1};
-                int dy[8]={0,0,1,-1,1,-1,1,-1};
-                for(int i=0;i<8;i++){
-                    int nx=x+dx[i];
-                    int ny=y+dy[i];
-                    if(nx>=0 && nx<n && ny>=0 && ny<n && g[nx][ny]==0){
-                        g[nx][ny]=1;
-                        q.push({nx,ny});
-                    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        
+        // Check if the starting or ending cell is blocked
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+            return -1;
+        }
+        
+        // Array to store the directions for 8 neighbors
+        int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+        
+        // Create a queue for BFS traversal
+        queue<pair<int, int>> q;
+        
+        // Start from the top-left cell and mark it as visited
+        q.push({0, 0});
+        grid[0][0] = 1;
+        
+        // Perform BFS
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            
+            // Check if reached the bottom-right cell
+            if (x == n - 1 && y == n - 1) {
+                return grid[x][y];
+            }
+            
+            // Explore all 8 neighbors
+            for (int i = 0; i < 8; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                // Check if the neighbor is within the grid boundaries
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 0) {
+                    q.push({nx, ny});
+                    grid[nx][ny] = grid[x][y] + 1;
                 }
             }
         }
+        
+        // No clear path found
         return -1;
     }
 };
