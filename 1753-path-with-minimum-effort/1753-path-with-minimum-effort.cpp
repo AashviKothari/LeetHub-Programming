9099ccd1e6b70348256c0f804967439f6a514772
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-06-12 12:17:51 +0530

    Time: 156 ms (80.49%), Space: 20 MB (69.73%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -3,66 +3,45 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         
         priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>>
             pq;
 
         int n = heights.size();
         int m = heights[0].size();
 
-        // Create a distance matrix with initially all the cells marked as
-        // unvisited and the dist for source cell (0,0) as 0.
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        dist[0][0] = 0;
-        pq.push({0, {0, 0}});
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        dist[0][0]=0;
+        pq.push({0,{0,0}});
 
-        // The following delta rows and delts columns array are created such that
-        // each index represents each adjacent node that a cell may have 
-        // in a direction.
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
+        int dr[]={-1,0,+1,0};
+        int dc[]={0,+1,0,-1};
 
-        // Iterate through the matrix by popping the elements out of the queue
-        // and pushing whenever a shorter distance to a cell is found.
-        while (!pq.empty())
-        {
+        while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
+
             int diff = it.first;
-            int row = it.second.first;
-            int col = it.second.second;
+            int r = it.second.first;
+            int c = it.second.second;
 
-            // Check if we have reached the destination cell,
-            // return the current value of difference (which will be min).
-            if (row == n - 1 && col == m - 1)
+            if(r==n-1 && c ==m-1){
                 return diff;
-           
-            for (int i = 0; i < 4; i++)
-            {
-                // row - 1, col
-                // row, col + 1
-                // row - 1, col
-                // row, col - 1
-                int newr = row + dr[i];
-                int newc = col + dc[i];
+            }
 
-                // Checking validity of the cell.
-                if (newr >= 0 && newc >= 0 && newr < n && newc < m)
-                {
-                    // Effort can be calculated as the max value of differences 
-                    // between the heights of the node and its adjacent nodes.
-                    int newEffort = max(abs(heights[row][col] - heights[newr][newc]), diff);
+            for(int i=0; i<4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-                    // If the calculated effort is less than the prev value
-                    // we update as we need the min effort.
-                    if (newEffort < dist[newr][newc])
-                    {
-                        dist[newr][newc] = newEffort;
-                        pq.push({newEffort, {newr, newc}});
-                    }
+                if(nc>=0 && nc<m && nr>=0 && nr<n){
+                    int ne = max(abs(heights[r][c] - heights[nr][nc]),diff);
+                if(ne < dist[nr][nc]) {
+                    dist[nr][nc]=ne;
+                    pq.push({ne,{nr,nc}});
+                }   
                 }
             }
         }
-        return 0; // if unreachable
+        return 0;
     }
 };
