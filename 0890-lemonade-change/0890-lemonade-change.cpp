b845c6f19c8347bba8c720b7a306688e2cde6084
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-06-15 02:26:00 +0530

    Time: 142 ms (5.79%), Space: 83.4 MB (31.6%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five = 0;
+        int five =0;
         int ten = 0;
 
-        for(int i = 0;i < bills.size();i++){
-            if(bills[i]==5) five++;
+        for(int i=0;i <bills.size();i++){
+            if(bills[i]==5){
+                five++;
+            }
             else if(bills[i]==10){
-                ten++;
-
-                if(five == 0) return false;
-                else{
+                if(five>0){
                     five--;
+                    ten++;
+                }
+                else{
+                    return false;
                 }
             }
-
             else{
-                if(ten > 0 && five > 0){
+                if(ten>0 && five>0){
                     ten--;
                     five--;
                 }
                 else if(five>=3){
-                    five-=3;
+                    five -=3;
                 }
                 else{
                     return false;
@@ -29,6 +31,5 @@ public:
             }
         }
         return true;
-        
     }
 };
