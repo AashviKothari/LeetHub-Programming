52ef0ad8092e8af43852f02b4fc12c50bdf37406
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-06-15 16:16:13 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -8,41 +8,40 @@ class Solution
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    
     struct meeting{
-            int start;
-            int end;
-            int pos;
-        };
-    static bool comp(meeting m1,meeting m2){
-        if(m1.end < m2.end) return true;
+        int start;
+        int end;
+        int pos;
+    };
+    
+    static bool comp(meeting m1, meeting m2){
+        if(m1.end<m2.end) return true;
         else if(m1.end>m2.end) return false;
         else if(m1.pos<m2.pos) return true;
         return false;
     }
     int maxMeetings(int start[], int end[], int n)
     {
-        
         meeting m[n];
         for(int i=0;i<n;i++){
+            m[i].start=start[i];
             m[i].end = end[i];
-            m[i].start = start[i];
-            m[i].pos = i+1;
+            m[i].pos=i+1;
         }
         
         sort(m,m+n,comp);
-        int endLimit = m[0].end;
-        int ans=1;
         
-        for(int i = 1; i<n;i++){
-            if(m[i].start>endLimit){
-                endLimit=m[i].end;
+        int limit = m[0].end;
+        int ans =1;
+        
+        for(int i=1;i<n;i++){
+            if(m[i].start>limit){
+                limit=m[i].end;
                 ans++;
             }
         }
         return ans;
         // Your code here
-        
     }
 };
 
