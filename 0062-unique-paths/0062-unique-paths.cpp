86bc0534796a48c138e604e4f035d0f61ec78462
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-07-08 13:13:01 +0530

    Time: 0 ms (100%), Space: 6.4 MB (49.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,51 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>&dp){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i < 0 || j < 0){
-            return 0;
-        }
+    // int f(int i, int j, vector<vector<int>>&dp){
+    //     if(i == 0 && j == 0){
+    //         return 1;
+    //     }
+    //     if(i < 0 || j < 0){
+    //         return 0;
+    //     }
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int up = f(i-1,j,dp);
-        int dw = f(i,j-1,dp);
+    //     int up = f(i-1,j,dp);
+    //     int dw = f(i,j-1,dp);
 
-        return dp[i][j] = up+dw;
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp);
-    }
+    //     return dp[i][j] = up+dw;
+    // }
+    int countWaysUtil(int m, int n, vector<vector<int> > &dp) {
+  for(int i=0; i<m ;i++){
+      for(int j=0; j<n; j++){
+          
+          //base condition
+          if(i==0 && j==0){
+              dp[i][j]=1;
+              continue;
+          }
+          
+          int up=0;
+          int left = 0;
+          
+          if(i>0) 
+            up = dp[i-1][j];
+          if(j>0)
+            left = dp[i][j-1];
+            
+          dp[i][j] = up+left;
+      }
+  }
+  
+  return dp[m-1][n-1];
+
+  
+}
+
+int uniquePaths(int m, int n){
+    vector<vector<int> > dp(m,vector<int>(n,-1));
+    return countWaysUtil(m,n,dp);
+    
+}
+
 };
