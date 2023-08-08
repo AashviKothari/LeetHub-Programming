3c30a04d4b91e743155636928af502001869cd86
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-08-08 10:59:25 +0530

    Time: 1535 ms (51.13%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -1,6 +1,6 @@
 select p.product_name, sum(o.unit) as unit
 from products p
-left join orders o
+RIGHT join orders o
 on p.product_id = o.product_id
 where o.order_date between '2020-02-01' and '2020-02-29'
 group by o.product_id
