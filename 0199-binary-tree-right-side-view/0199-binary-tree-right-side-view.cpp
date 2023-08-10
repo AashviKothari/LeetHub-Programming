28ce2e47e8683acdc29356299f87da7e495686c0
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-08-10 18:54:12 +0530

    Time: 2 ms (70.66%), Space: 12 MB (59.22%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,33 +12,34 @@
 class Solution {
 public:
 vector <int>rightSideView(TreeNode* root) {
-std::vector<int> result;
-    if (!root) {
-        return result;
-    }
+vector<int>ans;
+if(!root) 
+    return ans;
 
-    std::queue<TreeNode*> q;
-    q.push(root);
+queue<TreeNode*> q;
+q.push(root);
 
-    while (!q.empty()) {
-        int levelSize = q.size();
-        for (int i = 0; i < levelSize; ++i) {
-            TreeNode* node = q.front();
-            q.pop();
+while(!q.empty()){
+    int n = q.size(),x=0;
 
-            if (i == levelSize - 1) {
-                result.push_back(node->val);
-            }
-            if (node->left) {
-                q.push(node->left);
-            }
-            if (node->right) {
-                q.push(node->right);
-            }
-        }
-    }
+for(int i=0; i<n; i++){
 
-    return result;
+    TreeNode *node = q.front();
+    q.pop();
 
+    x = node->val;
+
+    if(i == n-1) //current value of element is the value of last visited element in the level of stack
+    ans.push_back(x);
+
+if(node->left) q.push(node->left);
+if(node->right) q.push(node->right);
+
+}
+
+// ans.push_back(x);
+}
+
+return ans;
 }
 };
