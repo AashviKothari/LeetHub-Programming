87e305aad3f8177ddfe4c90f9b918c4d3615e199
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-08-15 18:07:46 +0530

    Time: 7 ms (32.16%), Space: 14.8 MB (38.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
-    void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
+    void f(int open, int close, int n, vector<string> &ans, string curr){
+        if(curr.size()==n*2){
+            ans.push_back(curr);
             return;
         }
-        //when count of open and close brackets are same then 
-        //we have only one choice to put open bracket 
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else if(open == 0){
-            //only choice is to put close brackets 
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
-        }
-        else if(close == 0){
-            //only choise is to use open bracket 
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
-        }
+        
+        if(open<n) f(open+1, close, n, ans, curr+"(");
+        if(close<open) f(open, close+1, n, ans, curr+")");
     }
+    
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
         vector<string> ans;
-        string op = "";
-        solve(op, open, close, ans);
+        f(0, 0, n, ans, "");
         return ans;
     }
 };
