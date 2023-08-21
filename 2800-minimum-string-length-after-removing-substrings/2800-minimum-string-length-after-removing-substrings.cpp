d0f6a87444454605898be2613cec068e7e699abb
Aashvi Kothari <93025822+AashviKothari@users.noreply.github.com>
2023-08-21 11:20:16 +0530

    Time: 3 ms (93.83%), Space: 7.8 MB (78.68%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int minLength(string s) {
         stack<char>st;
-        for(auto x:s)
-        {
-        if(!st.empty() and ((x=='B' and st.top()=='A') || (x=='D' and  st.top()=='C')))
+        for( auto it : s){ 
+            if(!st.empty() and ((it == 'B' and st.top()=='A') || (it == 'D' and st.top() == 'C'))){
                 st.pop();
-            else
-                 st.push(x);           
+            }
+            else{
+                st.push(it);
+            }
         }
-         return st.size();
+        return st.size();
     }
 };
